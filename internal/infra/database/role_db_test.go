786ef1a7565fd119f09933ba9970d750285d4902
Mateus Lucena <mateusfaustino2017@gmail.com>
2025-07-18 20:17:33 -0300

    Adjust RoleMiddleware to handle role IDs

--- internal/infra/database/role_db_test.go
+++ internal/infra/database/role_db_test.go
@@ -56,10 +56,27 @@ func TestFindRoleByEmail(t *testing.T) {
 
 	assert.Nil(t, err)
 
-	
 	roleFound, err := roleDB.FindRoleByName(role.Name)
 
 	assert.Nil(t, err)
 	assert.Equal(t, role.ID, roleFound.ID)
 	assert.Equal(t, role.Name, roleFound.Name)
+}
+
+func TestFindRoleByID(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.Role{})
+
+	role, _ := entity.NewRole("tester")
+	roleDB := NewRoleDB(db)
+	err = roleDB.CreateRole(role)
+	assert.Nil(t, err)
+
+	found, err := roleDB.FindRoleByID(role.ID.String())
+	assert.Nil(t, err)
+	assert.Equal(t, role.ID, found.ID)
+	assert.Equal(t, role.Name, found.Name)
 }
